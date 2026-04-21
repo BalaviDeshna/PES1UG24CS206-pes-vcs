25607ce046887af814a739854b74c663a701b52e
deshna <deshna@cnLabXubuntu.myguest.virtualbox.org>
2026-04-21 19:41:28 +0530

    implement index_load

--- index.c
+++ index.c
@@ -138,7 +138,30 @@ int index_load(Index *index) {
     // TODO: Implement index loading
     // (See Lab Appendix for logical steps)
     (void)index;
-    return -1;
+    FILE *f = fopen(".pes/index", "r");
+    index->count = 0;
+
+    if (!f) return 0;  // empty index is OK
+
+    char hash_hex[HASH_HEX_SIZE + 1];
+
+    while (index->count < MAX_INDEX_ENTRIES) {
+        IndexEntry *e = &index->entries[index->count];
+
+        if (fscanf(f, "%o %64s %ld %ld %s\n",
+                &e->mode,
+                hash_hex,
+                &e->mtime_sec,
+                &e->size,
+                e->path) != 5)
+            break;
+
+        hex_to_hash(hash_hex, &e->hash);
+        index->count++;
+    }
+
+    fclose(f);
+    return 0;
 }
 
 // Save the index to .pes/index atomically.
